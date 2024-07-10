e4129ca4395211b48af8b01f0b0d18ed1f65c542
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2024-07-10 15:46:39 -0500

    Fix packages
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

--- com.example.matsample.plugin/src/com/example/matsample/queries/SampleQuery.java
+++ com.example.matsample.plugin/src/com/example/matsample/queries/SampleQuery.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.example.matsample;
+package com.example.matsample.queries;
 
 import org.eclipse.mat.query.IQuery;
 import org.eclipse.mat.query.IResult;
--- com.example.matsample.plugin/src/com/example/matsample/resolvers/SampleResolver.java
+++ com.example.matsample.plugin/src/com/example/matsample/resolvers/SampleResolver.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.example.matsample;
+package com.example.matsample.resolvers;
 
 import org.eclipse.mat.snapshot.extension.IClassSpecificNameResolver;
 import org.eclipse.mat.snapshot.extension.Subject;
